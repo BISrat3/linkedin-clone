8f955de177f07665d6bcda22bc98ed44bfe5999c
bisrat <bisrat.amtataw@gmail.com>
2023-01-28 10:10:26 -0500

    header

--- src/Header.js
+++ src/Header.js
@@ -1,22 +1,34 @@
 import React from 'react'
 import './Header.css'
 import SearchIcon from '@mui/icons-material/Search';
+import HeaderOption from './HeaderOption';
+import HomeIcon from '@mui/icons-material/Home';
+import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
+import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
+import ChatIcon from '@mui/icons-material/Chat';
+import NotificationsIcon from '@mui/icons-material/Notifications';
 
 function Header() {
   return (
     <div className='header'>
-        Header test
-        <div class="header__left">
-            <img src='https://cdn-icons-png.flaticon.com/512/174/174857.png' alt=''/>
-            <div class="header__search">
+        <div className="header__left">
+            <img src='https://cdn-icons-png.flaticon.com/512/174/174857.png'/>
+            <div className="header__search">
                 <SearchIcon/>
                 {/* SearchIcon */}
                 <input type="text"/>
             </div>
         </div>
 
-        <div class="header__right">
-
+        <div className="header__right">
+            {/* reusable components */}
+            {/* header options */}
+            <HeaderOption Icon={HomeIcon} title='Home'/>
+            <HeaderOption Icon={SupervisorAccountIcon} title='My Network'/>
+            <HeaderOption Icon={BusinessCenterIcon} title='Jobs'/>
+            <HeaderOption Icon={ChatIcon} title='Message'/>
+            <HeaderOption Icon={NotificationsIcon} title='Notifications'/>
+            <HeaderOption avatar='https://avatars.githubusercontent.com/u/99110345?v=4' title='me'/>
         </div>
     </div>
   )
---
+++ src/HeaderOption.js
@@ -0,0 +1,23 @@
+import { Avatar } from '@mui/material'
+import React from 'react'
+import './HeaderOption.css'
+
+function HeaderOption({Icon, avatar, title}) {
+    // properties allow us we can reuse the component again
+  return (
+    <div class="headerOption">
+        {/* if i pass an Icon should i render the icon component   */}
+        {Icon && <Icon className='headerOption__icon'/>}
+        {/* if it is avatar then render the component below */}
+        {avatar && (
+            <Avatar 
+                className='headerOption__icon'
+                src={avatar}
+            />
+        )}
+        <h3 className='headerOption__title'>{title}</h3>
+    </div>
+  )
+}
+
+export default HeaderOption

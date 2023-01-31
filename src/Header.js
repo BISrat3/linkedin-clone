dec7bde50a9a35a223e642639ea3dc72ff84c2cc
bisrat <bisrat.amtataw@gmail.com>
2023-01-31 07:42:51 -0500

    signout

--- src/Header.js
+++ src/Header.js
@@ -7,8 +7,17 @@ import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
 import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
 import ChatIcon from '@mui/icons-material/Chat';
 import NotificationsIcon from '@mui/icons-material/Notifications';
+import { useDispatch } from 'react-redux';
+import { logout } from "./features/userSlice"
+import { auth } from "./firebase"
 
 function Header() {
+  const dispatch = useDispatch()
+  
+  const logoutOfApp = () => {
+    dispatch(logout())
+    auth.signout()
+  }
   return (
     <div className='header'>
         <div className="header__left">
@@ -28,7 +37,11 @@ function Header() {
             <HeaderOption Icon={BusinessCenterIcon} title='Jobs'/>
             <HeaderOption Icon={ChatIcon} title='Message'/>
             <HeaderOption Icon={NotificationsIcon} title='Notifications'/>
-            <HeaderOption avatar='https://avatars.githubusercontent.com/u/99110345?v=4' title='me'/>
+            <HeaderOption 
+              avatar='https://avatars.githubusercontent.com/u/99110345?v=4' 
+              title='me'
+              onClick={logoutOfApp}
+              />
         </div>
     </div>
   )
--- src/HeaderOption.js
+++ src/HeaderOption.js
@@ -2,10 +2,12 @@ import { Avatar } from '@mui/material'
 import React from 'react'
 import './HeaderOption.css'
 
-function HeaderOption({Icon, avatar, title}) {
+function HeaderOption({Icon, avatar, title, onClick}) {
     // properties allow us we can reuse the component again
   return (
-    <div class="headerOption">
+    <div  
+      onClick ={onClick} 
+      class="headerOption">
         {/* if i pass an Icon should i render the icon component   */}
         {Icon && <Icon className='headerOption__icon'/>}
         {/* if it is avatar then render the component below */}
